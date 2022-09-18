c492a4eb7ab21560fa8b30c896eff6215acae23d
Maria Silvia <mariasrr97@gmail.com>
2022-09-17 22:18:32 -0300

    add tests

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -4,4 +4,26 @@ class TodoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "item normal" do
+    item = todos(:one)
+    assert item.save
+  end
+
+  test "item normal com status 1" do
+    item = todos(:two)
+    assert item.save
+  end
+
+  test "item sem nome" do
+    item = todos(:name_empty)
+    assert !item.save
+  end
+  
+  test "item sem status" do
+    item = todos(:status_empty)
+    assert item.save
+    # to do: deveria inferir 0
+  end
+  
 end
